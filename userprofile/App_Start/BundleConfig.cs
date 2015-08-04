a76760ad6751cba235898bebd2edda7207228aa8
zhaokk <994979136kk@gmail.com>
2015-08-04 22:21:22 +0800

    datepickerLocationpicker

--- userprofile/App_Start/BundleConfig.cs
+++ userprofile/App_Start/BundleConfig.cs
@@ -8,8 +8,10 @@ namespace userprofile
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
+            //bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+            //            "~/Scripts/jquery-{version}.js"));
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                      "~/Scriptsjquery-1.10.2.min.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
