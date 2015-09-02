19f943a5a420264e28408daa98289b9dd819d4e7
pockywei <sam-123-123@163.com>
2015-09-02 21:53:22 +1000

    change_login_page

--- userprofile/Scripts/login.js
+++ userprofile/Scripts/login.js
@@ -1,6 +1,7 @@
-﻿$("#login-button").click(function (event) {
+﻿$(".login").click(function (event) {
     event.preventDefault();
-
+    document.forms["sp2"].submit();
     $('form').fadeOut(500);
     $('.wrapper').addClass('form-success');
+    
 });
