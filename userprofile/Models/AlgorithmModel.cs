cad5d2668570fa7d80fc7b4a779c697fa0c84867
pockywei <sam-123-123@163.com>
2015-09-09 22:07:30 +1000

    fix the layout of the table

---
+++ userprofile/Models/AlgorithmModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace userprofile.Models
+{
+    public class AlgorithmModel
+    {
+        public List<solution> result { get; set; }
+        public AlgorithmModel()
+        {
+            this.result = new List<solution>();
+        }
+
+    }
+    public class solution
+    {
+        public List<pair> pairs { get; set; }
+    }
+    public class pair
+    {
+        int refID { get; set; }
+        int offerID { get; set; }
+    }
+}
--- userprofile/Scripts/AdminLTE/plugins/datatables/jquery.dataTables.js
+++ userprofile/Scripts/AdminLTE/plugins/datatables/jquery.dataTables.js
@@ -9287,7 +9287,7 @@
 			 *      } );
 			 *    } );
 			 */
-			"sInfo": "Showing _START_ to _END_ of _TOTAL_ entries",
+			"sInfo": "Showing _START_ to _END_ of _TOTAL_",
 		
 		
 			/**
