6e52002a309edaa16e9cd9d707eea3b67477665f
zhaokk <994979136kk@gmail.com>
2015-10-02 20:41:47 +0800

    commit

---
+++ userprofile/Models/MmatchViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace userprofile.Models
+{
+    public class MmatchViewModel
+    {
+        //public MmatchViewModel() { 
+        //this.offers=new offerpair[10];
+        //}
+       [Display(Name = "Offer Number")]
+        public int offernum { get; set; }
+        [Display(Name = "Created Match")]
+        public MATCH createdMatch { get; set; }
+      //  public LOCATION newlocation { get; set; }
+
+        public offerpair[] offers { get; set; }
+    }
+    public class offerpair
+    {
+        public int q { get; set; }
+        public string type { get; set; }
+
+        public int level { get; set; }
+    }
+    
+}
