1817d59e7baf862baa2d23bc9444e97d4ac94aa0
glen <gaw844@uowmail.edu.au>
2015-10-11 17:09:56 +1100

    Connors merge fixes

--- userprofile/Models/OrgViewModels.cs
+++ userprofile/Models/OrgViewModels.cs
@@ -5,22 +5,26 @@ using System.Web;
 
 namespace userprofile.Models
 {
-    
+
     public class OrgViewModels
     {
-        public OrgViewModels(Raoconnection db,string userid) {
-            List<OFFER> offermanagedbyO=new List<OFFER>();
-           AspNetUser Orgnizer= db.AspNetUsers.Find(userid);
-          majorT=db.TOURNAMENTs.First(d => d.organizer == userid);
+        public OrgViewModels(Raoconnection db, string userid)
+        {
+            List<OFFER> offermanagedbyO = new List<OFFER>();
+            AspNetUser Orgnizer = db.AspNetUsers.Find(userid);
+            majorT = db.TOURNAMENTs.First(d => d.organizer == userid);
             //posible other tournamanes
-         foreach(MATCH m in  majorT.MATCHes){
-             foreach (OFFER o in m.OFFERs) {
-                 offermanagedbyO.Add(o);
-             }
-         }
-       orgOVM = new admineOfferViewModel(offermanagedbyO);
+            foreach (MATCH m in majorT.MATCHes)
+            {
+                foreach (OFFER o in m.OFFERs)
+                {
+                    offermanagedbyO.Add(o);
+                }
+            }
+            orgOVM = new admineOfferViewModel(offermanagedbyO);
         }
-        public OrgViewModels() {
+        public OrgViewModels()
+        {
         }
 
 
--- userprofile/Models/managerViewModels.cs
+++ userprofile/Models/managerViewModels.cs
@@ -1,20 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
 namespace userprofile.Models
 {
-
-    public class managerViewModels
+    public class MmatchViewModel
     {
+        //public MmatchViewModel() { 
+        //this.offers=new offerpair[10];
+        //}
+        [Display(Name = "Offer Number")]
+        public int offernum { get; set; }
+        [Display(Name = "Created Match")]
+        public MATCH createdMatch { get; set; }
+        //  public LOCATION newlocation { get; set; }
 
-        public managerViewModels() { }
-
-        public managerViewModels(Raoconnection db,string userid) {
-            teams = db.TEAMs.Where(t => t.managerId == userid).ToList();
-          // teams= db.TEAMs.All(t => t.managerId == userid);
-        }
-        public List<TEAM> teams { get; set; }
+        public offerpair[] offers { get; set; }
     }
+    public class offerpair
+    {
+        public int q { get; set; }
+        public string type { get; set; }
+
+        public int level { get; set; }
+    }
+
 }
